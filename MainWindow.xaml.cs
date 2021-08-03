7f3caa7063ff87849d3230e0b1e310cfd3dd1ba0
Tomentos <thomas.gutknecht01@gmail.com>
2021-08-03 11:35:30 +0200

    Finished function that prevents double digit in Decimal Input
    If the value is 10 or higher, everything from the second number onwards is pushed behind the separator, the multiplicator gets increased by the amount of numbers pushed back and the value is recalculated accordingly.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -45,19 +45,42 @@ namespace ScientificNumberExtendor
             {
                 dec = float.Parse(Decimal_Input.Text);
 
+                // Check if the Decimal Input is double digits
                 if (dec >= 10)
                 {
-                    string[] oldDec;
-                    string[] newDec = new string[2];
 
+                    // Defining Variables for Double Digit Correction
+                    string[] oldDec = new string[2];
+                    string[] newDec = new string[2];
+                    string finDec;
+
+                    // Seperating the Decimal input into two arrays
                     if (dec.ToString().Contains(seperator))
                     {
                         oldDec = new string[2];
                         oldDec = dec.ToString().Split(seperator);
                     }
 
+                    else
+                    {
+                        oldDec[0] = dec.ToString();
+                        oldDec[1] = null;
+                    }
 
+                    // Entering corrected values into new string
+                    newDec[0] = oldDec[0].Remove(1);
+                    newDec[1] = oldDec[0].Remove(0, 1);
 
+                    if (oldDec[1] != null)
+                        newDec[1] += oldDec[1];
+
+                    // Parsing new values into Text Input and Global Variable
+                    finDec = (newDec[0] + seperator + newDec[1]);
+                    Decimal_Input.Text = finDec;
+                    dec = float.Parse(finDec);
+
+                    mul = mul + oldDec[0].Length - 1;
+                    Multiplicator_Input.Text = mul.ToString();
                 }
 
                 calculation();
